a427ff4a91655fb7623678d03f3cf57c774eb5e7
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-03-20 22:21:15 +0100

    jsx - introduction - expressions

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,36 @@ import "./App.scss";
 const App: FC = () => {
   const [count, setCount] = useState(0);
 
+  const values = [1, 2, 3, 4, 5, 6];
+  const text = values
+    .filter((el) => el % 2)
+    .map((el) => el ** 2)
+    .join("-");
+
+  const name = "stranger";
+
+  const transform = (name: string) => {
+    return name.toLocaleUpperCase();
+  };
+
+  const hello = <h1> Hello {transform(name)}! </h1>;
+
+  let greeting = <h1>Welcome my lord!</h1>;
+  const myName = prompt("Podaj swoje imię");
+
+  if (myName !== "Król Maciuś") {
+    greeting = <h1>You have no access to this place!</h1>;
+  }
+
   return (
     <>
-      <h1>Hello world from Vite!</h1>
+      <div> {hello} </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <div>{text}</div>
+        <div>{greeting}</div>
       </div>
     </>
   );
