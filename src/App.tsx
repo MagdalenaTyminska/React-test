c68eeaeb741ada58db28116670103d0c8bf1da20
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-03-20 22:37:40 +0100

    first exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,13 @@
-import { useState } from "react";
 import { FC } from "react"; //function component
 import "./App.scss";
 
 const App: FC = () => {
-  const [count, setCount] = useState(0);
-
-  const values = [1, 2, 3, 4, 5, 6];
-  const text = values
-    .filter((el) => el % 2)
-    .map((el) => el ** 2)
-    .join("-");
-
-  const name = "stranger";
-
-  const transform = (name: string) => {
-    return name.toLocaleUpperCase();
-  };
-
-  const hello = <h1> Hello {transform(name)}! </h1>;
-
-  let greeting = <h1>Welcome my lord!</h1>;
-  const myName = prompt("Podaj swoje imię");
-
-  if (myName !== "Król Maciuś") {
-    greeting = <h1>You have no access to this place!</h1>;
-  }
+  const first = 3;
+  const second = 4;
 
   return (
     <>
-      <div> {hello} </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div>{text}</div>
-        <div>{greeting}</div>
-      </div>
+      <h1>{first + second}</h1>
     </>
   );
 };
