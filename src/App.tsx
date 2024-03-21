503c5b40a1b306feb29b938fc082490abfe7de6c
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-03-21 14:39:34 +0100

    attributes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import { FC } from "react"; //function component
+import { CSSProperties, FC } from "react"; //function component
 import "./App.scss";
 
 const App: FC = () => {
-  const name = prompt("Provide your name");
+  const maxLength = 26;
+  const top = Math.round(Math.random() * 500);
+  const left = Math.round(Math.random() * 500);
+  const topSecond = Math.round(Math.random() * 100);
+  const leftSecond = Math.round(Math.random() * 100);
+
+  const style: CSSProperties = {
+    position: "absolute",
+    background: "green",
+  };
 
   return (
     <>
-      <h1> {name} </h1>
+      <div
+        className="section-primary"
+        data-additional-value={10}
+        aria-details="lorem ipsum"
+      />
+      <div
+        className="section-primary"
+        id="second-box"
+        style={{ ...style, top, left }}
+        // style={{
+        //   position: "absolute",
+        //   top,
+        //   left,
+        //   background: "green",
+        // }}
+      ></div>
+      <div
+        className="section-primary"
+        style={{ ...style, top: `${topSecond}vh`, left: `${leftSecond}vw` }}
+      ></div>
+      <label htmlFor="login">Login</label>
+      <input type="text" id="login" minLength={10} maxLength={maxLength} />
     </>
   );
 };
 
 export { App };
+
+// style inline - np. dla drag and drop
