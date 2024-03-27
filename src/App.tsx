4c0f8c9f3d71a06440e9ec1d514137e49e5edb0b
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-03-27 20:07:18 +0100

    3-key-nanoid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { FC, Fragment } from "react"; //function component
 import "./App.scss";
+import { nanoid } from "nanoid";
 
 const App: FC = () => {
   const numbers = [
-    { id: "4567uytd", val: 1 },
-    { id: "4589uytv", val: 2 },
-    { id: "4543uytm", val: 3 },
-    { id: "4511uytu", val: 4 },
-    { id: "4523uylr", val: 5 },
-    { id: "4987uytp", val: 6 },
+    { id: nanoid(), val: 1 },
+    { id: nanoid(), val: 2 },
+    { id: nanoid(), val: 3 },
+    { id: nanoid(), val: 4 },
+    { id: nanoid(), val: 5 },
+    { id: nanoid(), val: 6 },
   ];
 
   return numbers
@@ -16,6 +17,7 @@ const App: FC = () => {
     .map((number) => (
       <Fragment key={number.id}>
         <strong>{number.val}</strong>
+        <span>{number.id}</span>
       </Fragment>
     ));
 };
