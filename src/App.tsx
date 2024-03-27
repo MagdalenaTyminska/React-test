8b5ddbfe9552c37fe9ac0ca41ff9d5b23bb7183f
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-03-27 19:40:17 +0100

    1-key

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import { FC } from "react"; //function component
 import "./App.scss";
 
 const App: FC = () => {
-  let content = [<h4>"Wrong color"</h4>, <p>"change"</p>]; // można zapisać jako tablicę, <Fragment> lub <>
-
-  const names = ["Ala", "Ania", "Kamil", "Michał"];
-  const namesList = names.map((name) => <li>{name}</li>);
-
-  const numbers = [1, 2, 3, 4, 5, 6];
-  const numbersList = numbers
-    .filter((number) => number % 2 === 0)
-    .map((number) => <li>{number}</li>);
+  const users = [
+    { id: "jdnhdhe7600", name: "Ala" },
+    { id: "jdnhfrkhe76", name: "Ania" },
+    { id: "jnh987khe56", name: "Kamil" },
+    { id: "j65t87khe96", name: "Michał" },
+  ];
+  const usersList = users.map((user) => (
+    <li key={user.id}>
+      <strong>{user.name}</strong>
+    </li>
+  ));
 
   return (
     <>
-      {content}
-      <ul>{namesList}</ul>
-      <ul>{numbersList}</ul>
+      <ul>{usersList}</ul>
     </>
   );
 };
