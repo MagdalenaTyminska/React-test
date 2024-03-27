1ea709d0ad153434a3fb723362983dcce6499456
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-03-27 21:31:59 +0100

    2-ex

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,57 @@
+// import { FC } from "react"; //function component
+// import "./App.scss";
+// // import { nanoid } from "nanoid";
+
+// const App: FC = () => {
+//   const text = "Lorem ipsum";
+//   const textArray = [...text];
+//   const getLetter = textArray
+//     .filter((letter: string) => letter !== " ")
+//     .map((letter: string, index: number) => (
+//       <li key={index}>
+//         {index % 2 === 0
+//           ? letter.toLocaleLowerCase()
+//           : letter.toLocaleUpperCase()}
+//       </li>
+//     )); // konwertowanie do tablicy - dodanie do nowej tablicy
+
+//   return (
+//     <>
+//       <ul>{getLetter}</ul>
+//     </>
+//   );
+// };
+
+// export { App };
+
 import { FC } from "react"; //function component
 import "./App.scss";
-import { nanoid } from "nanoid";
-
-type SingleElement = {
-  id: string;
-  color: string;
-  size: number;
-};
+// import { nanoid } from "nanoid";
 
 const App: FC = () => {
-  const elements: SingleElement[] = [
-    { id: nanoid(), color: "blue", size: 10 },
-    { id: nanoid(), color: "green", size: 14 },
-    { id: nanoid(), color: "yellow", size: 18 },
-  ];
+  const text = "Lorem ipsum";
+  const textArray = [...text];
+  const getLetter = (letter: string, index: number) => {
+    if (letter === " ") {
+      return null;
+    }
 
-  return elements.map((element) => (
-    <div
-      key={element.id}
-      style={{
-        backgroundColor: element.color,
-        width: element.size,
-        height: element.size,
-      }}
-    />
-  ));
+    if (index % 2 === 0) return letter.toLocaleLowerCase();
+    return letter.toLocaleUpperCase();
+  };
+
+  return (
+    <>
+      <ul>
+        {textArray.map(
+          (letter: string, index: number) =>
+            getLetter(letter, index) !== null && (
+              <li key={index}>{getLetter(letter, index)}</li>
+            )
+        )}
+      </ul>
+    </>
+  );
 };
 
 export { App };
