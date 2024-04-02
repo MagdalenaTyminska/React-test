d9e7c1442d4033ddf424573739f00fc93f2866ff
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-04-02 18:29:08 +0200

    added class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,8 @@
-// import { FC } from "react"; //function component
-// import "./App.scss";
-// // import { nanoid } from "nanoid";
-
-// const App: FC = () => {
-//   const text = "Lorem ipsum";
-//   const textArray = [...text];
-//   const getLetter = textArray
-//     .filter((letter: string) => letter !== " ")
-//     .map((letter: string, index: number) => (
-//       <li key={index}>
-//         {index % 2 === 0
-//           ? letter.toLocaleLowerCase()
-//           : letter.toLocaleUpperCase()}
-//       </li>
-//     )); // konwertowanie do tablicy - dodanie do nowej tablicy
-
-//   return (
-//     <>
-//       <ul>{getLetter}</ul>
-//     </>
-//   );
-// };
-
-// export { App };
-
-import { FC } from "react"; //function component
 import "./App.scss";
-// import { nanoid } from "nanoid";
+import { HelloWorld } from "./HelloWorld";
 
-const App: FC = () => {
-  const text = "Lorem ipsum";
-  const textArray = [...text];
-  const getLetter = (letter: string, index: number) => {
-    if (letter === " ") {
-      return null;
-    }
-
-    if (index % 2 === 0) return letter.toLocaleLowerCase();
-    return letter.toLocaleUpperCase();
-  };
-
-  return (
-    <>
-      <ul>
-        {textArray.map(
-          (letter: string, index: number) =>
-            getLetter(letter, index) !== null && (
-              <li key={index}>{getLetter(letter, index)}</li>
-            )
-        )}
-      </ul>
-    </>
-  );
+const App = () => {
+  return <HelloWorld />;
 };
 
 export { App };
---
+++ src/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class HelloWorld extends Component {
+  render() {
+    return <h1>Hello world!</h1>;
+  }
+}
+
+export { HelloWorld };
