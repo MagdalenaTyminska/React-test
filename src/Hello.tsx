825f49e2f403eb507e6973675f0ab27a018cb78c
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-04-03 20:56:10 +0200

    basic class

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,7 +1,19 @@
+// interface Props {
+//   name: string;
+// }
+
+// export const Hello = (props: Props) => {
+//   return <h1>Hello{props.name}</h1>;
+// };
+
+import { Component } from "react";
+
 interface Props {
   name: string;
 }
 
-export const Hello = (props: Props) => {
-  return <h1>Hello{props.name}</h1>;
-};
+export class Hello extends Component<Props> {
+  render() {
+    return <h1>Hello {this.props.name}</h1>;
+  }
+}
