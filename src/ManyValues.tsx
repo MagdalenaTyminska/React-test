d623b4e4df29f539e851f9de8fe706424ab324c7
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-04-03 21:25:47 +0200

    destrukturyzacja

--- src/ManyValues.tsx
+++ src/ManyValues.tsx
@@ -8,15 +8,39 @@ type Props = {
   seventh?: number;
 };
 
-const ManyValues = (props: Props) => {
-  const sum =
-    props.first +
-    props.second +
-    props.third +
-    props.fourth +
-    props.fifth +
-    props.sixth;
+const ManyValues = ({
+  first,
+  second,
+  third,
+  fourth,
+  fifth,
+  sixth,
+  seventh = 10,
+}: Props) => {
+  const sum = first + second + third + fourth + fifth + sixth + (seventh || 0);
   return <h1>{sum}</h1>;
 };
 
 export { ManyValues };
+
+// import { Component } from "react";
+
+// type Props = {
+//   first: number;
+//   second: number;
+//   third: number;
+//   fourth: number;
+//   fifth: number;
+//   sixth: number;
+//   seventh?: number;
+// };
+
+// class ManyValues extends Component<Props> {
+//   render() {
+//     const { first, second, third, fourth, fifth, sixth } = this.props;
+//     const sum = first + second + third + fourth + fifth + sixth;
+//     return <h1>{sum}</h1>;
+//   }
+// }
+
+// export { ManyValues };
