04ea7d31ac0f20736188d45a37b4b1a406df9999
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-04-11 21:16:55 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import { Container } from "./Container";
 import { Content } from "./Content";
+import { PageHeader } from "./PageHeader";
 
 const App = () => {
   return (
     <>
       <Container>
+        <PageHeader title={"Lorem ipsum"} left={<button>[-]</button>} />
         <Content />{" "}
       </Container>
       <Container>
