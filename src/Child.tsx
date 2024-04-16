98fe7a42569bf8784c3349ff6f74a22018c3d9a7
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-04-16 14:55:42 +0200

    event

--- src/Child.tsx
+++ src/Child.tsx
@@ -9,17 +9,20 @@ export const Child = ({ callback }: Props) => {
     console.log("Clicked from child", e);
   };
 
-  const handleAnotherClick = (identifier: string, e: SyntheticEvent) => {
-    console.log("Clicked from child", identifier, e);
+  const handleAnotherClick = (identifier: string) => {
+    console.log("Clicked from child", identifier);
+  };
+
+  const handleChange = (e: SyntheticEvent) => {
+    console.log((e.target as any).value);
   };
 
   return (
     <>
       <button onDoubleClick={callback}>Click</button>
       <button onDoubleClick={handleLocalClick}>Click</button>
-      <button onDoubleClick={(event) => handleAnotherClick("test-id", event)}>
-        Click
-      </button>
+      <button onDoubleClick={() => handleAnotherClick("test-id")}>Click</button>
+      <input name="test" id="test" type="text" onChange={handleChange} />
     </>
   );
 };
