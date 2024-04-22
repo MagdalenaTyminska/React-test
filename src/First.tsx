0953dc096b72e7044c33b407621d7f0dafa1b45d
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-04-22 15:05:38 +0200

    useEffect2

--- src/First.tsx
+++ src/First.tsx
@@ -1,17 +1,57 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const First = () => {
-  const [counter, setCounter] = useState(0);
+  const [people, setPeople] = useState<string[]>(["Tomek", "Franek"]);
 
-  useEffect(() => {
-    setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-  }, []);
+  const firstPerson = people[0];
+
+  const addPerson = () => {
+    setPeople((prevPeople) => [
+      `${Math.round(Math.random() * 1000)}`,
+      ...prevPeople,
+    ]);
+  };
 
   return (
     <>
-      <h1>{counter}</h1>
+      <button onClick={addPerson}>Add person</button>
+      <p>Pierwsza osoba: {firstPerson}</p>
+      <ul>
+        {people.map((person, index) => (
+          <li key={index}>{person}</li>
+        ))}
+      </ul>
     </>
   );
 };
+
+// niepoprawne użycie useEffect - nie jest potrzebne
+// import { useEffect, useState } from "react";
+
+// export const First = () => {
+//   const [people, setPeople] = useState<string[]>(["Tomek", "Franek"]);
+//   const [firstPerson, setFirstPerson] = useState(people[0]);
+
+//   useEffect(() => {
+//     setFirstPerson(people[0]);
+//   }, [people]);
+
+//   const addPerson = () => {
+//     setPeople((prevPeople) => [
+//       `${Math.round(Math.random() * 1000)}`,
+//       ...prevPeople,
+//     ]);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={addPerson}>Add person</button>
+//       <p>Pierwsza osoba: {firstPerson}</p>
+//       <ul>
+//         {people.map((person, index) => (
+//           <li key={index}>{person}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
