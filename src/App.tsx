8c610ab3c4419a3d9a0a850bcf1615c0fff4bade
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-04-22 10:13:21 +0200

    first call

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.scss";
 import { First } from "./First";
-import { Second } from "./Second";
-import { Third } from "./Third";
 
 const App = () => {
   return (
     <>
       <First />
-      <Second />
-      <Third />
     </>
   );
 };
