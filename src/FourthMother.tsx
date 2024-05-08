5f750a9ea717ae9890ec26b01410df6fd51051ce
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-05-08 21:43:39 +0200

    conditional-rendering-ex

---
+++ src/FourthMother.tsx
@@ -0,0 +1,20 @@
+import { FourthLight } from "./FourthLight";
+import { FourthDark } from "./FourthDark";
+import { useState } from "react";
+
+export const FourthMother = () => {
+  const [isTurnedOn, setTurnedOn] = useState(false);
+
+  const toggleLight = () => {
+    setTurnedOn((prevTurnedOn) => !prevTurnedOn);
+  };
+
+  return (
+    <>
+      <button onClick={toggleLight}>
+        {isTurnedOn ? `Turn off` : `Turn on`}
+      </button>
+      {isTurnedOn ? <FourthLight /> : <FourthDark />}
+    </>
+  );
+};
