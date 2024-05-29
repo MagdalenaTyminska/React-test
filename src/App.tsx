ce7fa41938f12a902add1657ff1f55e6e88ae228
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-05-29 18:41:48 +0200

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import "./App.scss";
-import { First } from "./First";
-import { Second } from "./Second";
-import { Third } from "./Third";
-import { FourthMother } from "./FourthMother";
-import { Fifth } from "./Fifth";
+import './App.scss';
+import { First } from './First';
+import { Second } from './Second';
+import { Third } from './Third';
+import { FourthMother } from './FourthMother';
+import { Fifth } from './Fifth';
 
 const App = () => {
   return (
     <>
       <First />
-      <Second user={{ name: "Magda", lastName: "Kowalska", age: 17 }} />
+      <Second user={{ name: 'Magda', lastName: 'Kowalska', age: 17 }} />
       <Third a={5} b={8} c={6} />
       <FourthMother />
       <Fifth />
