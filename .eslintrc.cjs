778fa3a5cccd3c30388af0d081a7d4d0d2092b16
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-06-03 19:01:55 +0200

    prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,18 @@ module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
 	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react-hooks/recommended",
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
 	],
-	ignorePatterns: ["dist", ".eslintrc.cjs"],
-	parser: "@typescript-eslint/parser",
-	plugins: ["react-refresh", "import"],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'import'],
 	rules: {
-		"react-refresh/only-export-components": [
-			"warn",
+		'react-refresh/only-export-components': [
+			'warn',
 			{ allowConstantExport: true },
 		],
-		"import/no-default-export": ["error"],
+		'import/no-default-export': ['error'],
 	},
 };
