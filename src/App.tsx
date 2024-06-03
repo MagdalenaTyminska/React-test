ea927447159c37923920f070ee2045e3f47aa3f2
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-06-03 11:29:02 +0200

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import "./App.scss";
-import { First } from "./First";
-import { Second } from "./Second";
-import { Third } from "./Third";
-import { FourthMother } from "./FourthMother";
-import { Fifth } from "./Fifth";
+import './App.scss';
+import { First } from './First';
+import { Second } from './Second';
+import { Third } from './Third';
+import { FourthMother } from './FourthMother';
+import { Fifth } from './Fifth';
 
 const App = () => {
-  return (
-    <>
-      <First />
-      <Second user={{ name: "Magda", lastName: "Kowalska", age: 17 }} />
-      <Third a={5} b={8} c={6} />
-      <FourthMother />
-      <Fifth />
-    </>
-  );
+	return (
+		<>
+			<First />
+			<Second user={{ name: 'Magda', lastName: 'Kowalska', age: 17 }} />
+			<Third a={5} b={8} c={6} />
+			<FourthMother />
+			<Fifth />
+		</>
+	);
 };
 
 export { App };
