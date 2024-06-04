c79896f61210fef84a04d560cd55ef509d7df0c2
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-06-04 21:30:39 +0200

    custom-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import './App.scss';
-import { First } from './First';
-import { Second } from './Second';
-import { Third } from './Third';
-import { FourthMother } from './FourthMother';
-import { Fifth } from './Fifth';
+import { useConsoleLog } from './hooks/useConsoleLog';
+import { useCustomLogger } from './hooks/useCustomLogger';
 
 const App = () => {
+	useConsoleLog();
+	// const { logCount, log } = useCustomLogger('Test_App');
+	// 	const logSth = () => {
+	// 		log(Math.round(Math.random() * 1000).toString());
+	// 	};
+
+	// 	return (
+	// 		<div>
+	// 			<h1>{logCount}</h1>
+	// 			<button onClick={logSth}>Log sth</button>
+	// 		</div>
+	// 	);
+	// };
+
+	const [logCount, log] = useCustomLogger('Test_App');
+
+	const logSth = () => {
+		log(Math.round(Math.random() * 1000).toString());
+	};
+
 	return (
-		<>
-			<First />
-			<Second user={{ name: 'Magda', lastName: 'Kowalska', age: 17 }} />
-			<Third a={5} b={8} c={6} />
-			<FourthMother />
-			<Fifth />
-		</>
+		<div>
+			<h1>{logCount}</h1>
+			<button onClick={logSth}>Log sth</button>
+		</div>
 	);
 };
 
--- src/FourthDark.tsx
+++
@@ -1,3 +0,0 @@
-export const FourthDark = () => {
-	return <div style={{ backgroundColor: 'blue', height: 100, width: 100 }} />;
-};
