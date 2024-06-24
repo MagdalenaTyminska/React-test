5f613acacef51ccc124d7587bce780aed8c80049
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-06-24 14:49:00 +0200

    Forms-hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.scss';
-import { HW1 } from './HW1';
-// import { HW2 } from './HW2';
+// import { HW1 } from './HW1';
+import { HW2 } from './HW2';
 
 export const App = () => {
 	return (
 		<>
-			<HW1 />
-			{/* <HW2 /> */}
+			{/* <HW1 /> */}
+			<HW2 />
 		</>
 	);
 };
