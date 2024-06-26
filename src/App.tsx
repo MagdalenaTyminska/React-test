a817de97f6f09eb43b028ed5583e43d9223cc1c1
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-06-26 18:20:46 +0200

    added Input component and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import { useRef } from 'react';
 import './App.scss';
+import { Input } from './Input';
 
 export const App = () => {
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	const handleClick = () => {
-		// niepoprawny zapis
-		// document.querySelector('input')?.select();
-		// const inputRef = useRef<HTMLInputElement>(null);
 		inputRef.current?.focus();
 	};
 
 	return (
 		<>
 			<button onClick={handleClick}>Focus</button>
-			<input type='text' ref={inputRef} />
+			<Input value='Lorem ipsum' passedRef={inputRef} />
 		</>
 	);
 };
