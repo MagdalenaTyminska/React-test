18ea2816e837c824ac8704c8f512be9cffb4dcae
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-07-23 16:32:15 +0200

    added List.tsx

--- src/List.tsx
+++ src/List.tsx
@@ -1,3 +1,31 @@
+import { useEffect } from 'react';
+import { useApi } from './hooks/useApi';
+
+export type Entity = {
+	id: string;
+	prop1: string;
+	prop2: number;
+};
+
 export const List = () => {
-	return null;
+	const { get, data, loading, error } = useApi<Entity[]>();
+
+	useEffect(() => {
+		get('collection1');
+	}, [get]);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>{error}</p>;
+	if (!data) return null;
+
+	return (
+		<ul>
+			{data.map((datum) => (
+				<li key={datum.id}>
+					{datum.prop1}
+					{datum.prop2}
+				</li>
+			))}
+		</ul>
+	);
 };
