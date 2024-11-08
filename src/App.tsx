cc6fd133f325a17c9efa262280e3a1f50c967a42
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-11-08 16:55:22 +0100

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,15 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import './App.scss';
-import { Button } from './Button';
-import { Text } from './Text';
-import { Wrapper } from './Wrapper';
-import { useState } from 'react';
-import { Bouncy } from './Bouncy';
-
-const GlobalStyle = createGlobalStyle`
-body {
-background-color: ${(props) => props.theme.colors.background}
-}
-`;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Hello } from './Hello';
 
 export const App = () => {
-	const [isLight, setLight] = useState(true);
-
-	const light = {
-		colors: {
-			primary: '#0333EE',
-			background: '#fff',
-			textPrimary: '#eee',
-			textBackground: '#333',
-		},
-	};
-
-	const dark = {
-		colors: {
-			primary: '#9ca5df',
-			background: '#000',
-			textPrimary: '#333',
-			textBackground: '#eee',
-		},
-	};
-
-	const toggleTheme = () => {
-		setLight((prevLight) => !prevLight);
-	};
-
 	return (
 		<>
-			<ThemeProvider theme={isLight ? light : dark}>
-				<GlobalStyle />
-				<Wrapper>
-					<Button label='click' onClick={toggleTheme} />
-					<Text>Lorem ipsum</Text>
-				</Wrapper>
-				<Bouncy />
-			</ThemeProvider>
+			<BrowserRouter>
+				<Routes />
+				<Route path='/' element={<Hello />} />
+				<Routes />
+			</BrowserRouter>
 		</>
 	);
 };
